e5697788f3257ea33a9069c483715ff17a50d39b
Dinwy <whix2010@gmail.com>
2016-01-21 00:51:00 +0900

    Add login

--- src/main/java/xyz/dinwy/blog/WebVerticle.java
+++ src/main/java/xyz/dinwy/blog/WebVerticle.java
@@ -4,7 +4,10 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
 
 public class WebVerticle extends AbstractVerticle {
 	@Override
@@ -13,6 +16,12 @@ public class WebVerticle extends AbstractVerticle {
 		HttpServer server = vertx.createHttpServer();
 		Router router = Router.router(vertx);
 
+	    router.route().handler(CookieHandler.create());
+	    router.route().handler(SessionHandler
+	        .create(LocalSessionStore.create(vertx))
+	        .setCookieHttpOnlyFlag(true)
+	        .setCookieSecureFlag(true)
+	    );
 		router.route("/").handler(StaticHandler.create().setWebRoot("../app-root/repo/webroot").setCachingEnabled(true));
 		router.route("/static/*").handler(StaticHandler.create().setWebRoot("../app-root/repo/webroot/static").setCachingEnabled(true));
 		router.route("/api/*").handler(StaticHandler.create("api").setCachingEnabled(false));
--- src/main/java/xyz/dinwy/blog/local/WebVerticle.java
+++ src/main/java/xyz/dinwy/blog/local/WebVerticle.java
@@ -3,8 +3,21 @@ package xyz.dinwy.blog.local;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.AuthProvider;
+import io.vertx.ext.auth.shiro.ShiroAuth;
+import io.vertx.ext.auth.shiro.ShiroAuthOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.AuthHandler;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.FormLoginHandler;
+import io.vertx.ext.web.handler.RedirectAuthHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.UserSessionHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
 
 public class WebVerticle extends AbstractVerticle {
 	@Override
@@ -12,7 +25,26 @@ public class WebVerticle extends AbstractVerticle {
 		
 		HttpServer server = vertx.createHttpServer();
 		Router router = Router.router(vertx);
+		
+	    router.route().handler(CookieHandler.create());
+	    router.route().handler(BodyHandler.create());
+	    router.route().handler(SessionHandler
+	        .create(LocalSessionStore.create(vertx))
+	        .setCookieHttpOnlyFlag(true)
+	        .setCookieSecureFlag(true)
+	    );
 
+	    JsonObject config = new JsonObject().put("properties_path", "misc/vertx-users.properties");
+	    ShiroAuthOptions SAO = new ShiroAuthOptions().setConfig(config).setType(ShiroAuthRealmType.PROPERTIES);
+	    AuthProvider authProvider = ShiroAuth.create(vertx, SAO);
+	    router.route().handler(UserSessionHandler.create(authProvider));
+//	    ShiroAuth Setting
+	    AuthHandler redirectAuthHandler = RedirectAuthHandler.create(authProvider,"/private/signin.html");
+
+	    router.route("/private/").handler(redirectAuthHandler);
+	    router.route("/private/*").handler(StaticHandler.create().setWebRoot("webroot/view").setCachingEnabled(false));
+	    router.route("/loginhandler").handler(FormLoginHandler.create(authProvider));
+	    
 		router.route("/").handler(StaticHandler.create().setWebRoot("webroot").setCachingEnabled(true));
 		router.route("/static/*").handler(StaticHandler.create().setWebRoot("webroot/static").setCachingEnabled(true));
 		router.route("/api/*").handler(StaticHandler.create("api").setCachingEnabled(false));
